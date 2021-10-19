c7589a7493ec3b6d9a610fd9d3fb0b7c8419caf5
ShubhamPV <shubhamvadnere5@gmail.com>
2021-10-19 12:48:06 +0530

    [ShubhamPV]

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistrationRegex user = new UserRegistrationRegex();
+            user.Validation();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserRegistration/UserRegistrationRegex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{/// <summary>
+/// UC1 creating regular expression for validating user first name
+/// </summary>
+    class UserRegistrationRegex
+    {
+        //writing the valid pattern
+        string pattern = "^[A-Z]{1}[a-z]{3,}$";
+        string[] inputs = { "shubh", "shubh?re", "Shubh96", "Shubham", "Shubhamvad@gmail.com", "CRETA", "sv" };//Validating some Example
+
+
+        /// <summary>
+        /// Validation this instance.
+        /// </summary>
+        public void Validation()
+        {
+            Regex regex1 = new Regex(pattern);
+            Console.WriteLine("Validating First name: ");
+            ItarateLoop(inputs, regex1);
+        }
+        public void ItarateLoop(string[] arr, Regex regex1)
+        {
+            for (int i = 0; i < arr.Length; i++)//validation Loop
+            {
+                bool result = regex1.IsMatch(arr[i]);
+                if (result)
+                {
+                    Console.WriteLine(arr[i] + ": is-->" + "Valid");
+                }
+                else
+                {
+                    Console.WriteLine(arr[i] + ": is-->" + "Invalid");
+                }
+            }
+        }
+    }
+}
